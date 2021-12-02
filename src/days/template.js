def45f807e5732c28c0e77193d97d777a308360b
Vallis <p.vallis@rasa.com>
2021-12-02 09:35:01 +0000

    Day 1 part 1

---
+++ src/days/template.js
@@ -0,0 +1,10 @@
+const testInput = [];
+
+const realInput = [];
+
+const one = (input) => () => "Not implemented";
+
+const two = (input) => () => "Not implemented";
+
+export const partOne = one(testInput);
+export const partTwo = two(testInput);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { partOne, partTwo } from "./days/1";
+
+console.log("Part 1: ", partOne());
+console.log("Part 2: ", partTwo());
